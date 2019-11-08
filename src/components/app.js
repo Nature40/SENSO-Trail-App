f3fb2146980fe0a723400f9f111f48a0105c3d31
nimo-19 <j.hofmann.nimo@gmail.com>
2019-11-08 19:32:53 +0100

    add simple link to start page

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
+import { Link } from 'react-router-dom'
 import '../styles/app.scss'
 import NoMatch from './noMatch.js'
 import TrailsListContainer from '../containers/trailslist.container.js'
@@ -12,7 +13,7 @@ function App () {
       </header>
       <main>
         <Switch>
-          <Route exact path='/' render={() => <div>HALLO WELT</div>} />
+    <Route exact path='/' render={() => <div>HALLO WELT <Link to="/trails" >GOTO Trails</Link></div>} />
           <Route exact path='/trails' component={TrailsListContainer} />
           <Route component={NoMatch} />
         </Switch>
