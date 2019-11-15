c37e7d25a61bc7a61047c72a69f6e93ac92ad491
nimo-19 <j.hofmann.nimo@gmail.com>
2019-11-15 10:25:56 +0100

    add station list and view tests

---
+++ src/__tests__/stationList.spec.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import StationList from '../components/stationList.js'
+
+/* eslint-env jest */
+
+describe('<StationList>', () => {
+  it('should render without crashing', () => {
+    shallow(<StationList stations={[]} />)
+  })
+
+  it('should render "Keine Stationen geladen" if no stations had been loaded', () => {
+    const wrapper = shallow(<StationList stations={[]} />)
+
+    expect(wrapper.find('p').text()).toEqual('Keine Stationen geladen')
+  })
+
+  it('should render "Station Views" if stations had been loaded', () => {
+    const stations = [
+      {
+        uuid: 'uuid1'
+      },
+      {
+        uuid: 'uuid2'
+      }
+    ]
+    const wrapper = shallow(<StationList stations={stations} />)
+
+    expect(wrapper.find('StationView')).toHaveLength(2)
+  })
+})
