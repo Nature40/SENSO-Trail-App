3d1fed278be2549c2bda4ff073c03ab1312b3a8d
Nimo-19 <Nimo-19@users.noreply.github.com>
2019-11-22 17:37:30 +0000

    Automated deployment: Fri Nov 22 17:37:30 UTC 2019 64771cc0fd8c110a3f986898cc19fb62c7b0e609

---
+++ precache-manifest.8c4160bf965d69d1d202616681c23de2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c65b010eb0585289ab409bf3980f363",
+    "url": "/SENSO-Trail-App/index.html"
+  },
+  {
+    "revision": "e0666b56200a3594b1d0",
+    "url": "/SENSO-Trail-App/static/css/main.ccf41563.chunk.css"
+  },
+  {
+    "revision": "0aaf6ac4caa92072f53b",
+    "url": "/SENSO-Trail-App/static/js/2.7c178de0.chunk.js"
+  },
+  {
+    "revision": "e0666b56200a3594b1d0",
+    "url": "/SENSO-Trail-App/static/js/main.ae4c8f70.chunk.js"
+  },
+  {
+    "revision": "26d361b54751880f5b60",
+    "url": "/SENSO-Trail-App/static/js/runtime-main.b8c2dca3.js"
+  },
+  {
+    "revision": "8fb0645c655110c14abc0f146767c126",
+    "url": "/SENSO-Trail-App/static/media/background-test.8fb0645c.png"
+  },
+  {
+    "revision": "7585105bfa694a1c22deb1565a3380f0",
+    "url": "/SENSO-Trail-App/static/media/correct.7585105b.svg"
+  },
+  {
+    "revision": "4a77c0d8b5684079852f1dc17dfd4463",
+    "url": "/SENSO-Trail-App/static/media/simple-forest.4a77c0d8.svg"
+  },
+  {
+    "revision": "d262446d628e4dfdcfd4c39f4134c47a",
+    "url": "/SENSO-Trail-App/static/media/wrong.d262446d.svg"
+  }
+]);
