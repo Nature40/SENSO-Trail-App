43fe0ff272b9c35505afde6d190d47dc1ac404b2
Nimo-19 <Nimo-19@users.noreply.github.com>
2020-04-03 06:10:01 +0000

    Automated deployment: Fri Apr  3 06:10:01 UTC 2020 4bda6219e3b501966609bab5841bbea292655796

---
+++ precache-manifest.7c666497c5659b5f9d33b8f3105cec0c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd47b93db313ef13b1aa4dbe2e4270d",
+    "url": "SENSO-Trail-App/index.html"
+  },
+  {
+    "revision": "22425603235af5d0d2b5",
+    "url": "SENSO-Trail-App/static/css/main.b462a5e6.chunk.css"
+  },
+  {
+    "revision": "f9e273397684bb720d3c",
+    "url": "SENSO-Trail-App/static/js/2.e1df5185.chunk.js"
+  },
+  {
+    "revision": "e597467df4a17bc45ecf02e5eb970cc7",
+    "url": "SENSO-Trail-App/static/js/2.e1df5185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22425603235af5d0d2b5",
+    "url": "SENSO-Trail-App/static/js/main.0e22a391.chunk.js"
+  },
+  {
+    "revision": "c18d52e1af7da30192b3",
+    "url": "SENSO-Trail-App/static/js/runtime-main.38afa321.js"
+  },
+  {
+    "revision": "8fb0645c655110c14abc0f146767c126",
+    "url": "SENSO-Trail-App/static/media/background-test.8fb0645c.png"
+  },
+  {
+    "revision": "101c9dabcf97a91f7ceff3d32da52cb6",
+    "url": "SENSO-Trail-App/static/media/correct.101c9dab.svg"
+  },
+  {
+    "revision": "4a77c0d8b5684079852f1dc17dfd4463",
+    "url": "SENSO-Trail-App/static/media/simple-forest.4a77c0d8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://nature40.github.io/SENSO-Trail-App/precache-manifest.fb34187ae96402e6431c2954996dfdb3.js"
+  "SENSO-Trail-App/precache-manifest.7c666497c5659b5f9d33b8f3105cec0c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://nature40.github.io/SENSO-Trail-App/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("SENSO-Trail-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
