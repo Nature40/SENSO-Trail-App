d55bee428dd47ab19bff66d55b15789762f80f81
Nimo <j.hofmann.nimo@gmail.com>
2020-04-03 07:59:25 +0200

    simple map test

---
+++ src/components/mapIframe.view.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Map ({
+
+}) {
+  return (
+    <div>
+      <iframe id="inlineFrameExample"
+        title="Inline Frame Example"
+        width="1000"
+        height="1000"
+        src="https://nature40.github.io/GPS-TotalStation-Documentation/assets/maps/cst_leaflet/cst_webmap.html">
+      </iframe>
+    </div>
+  )
+}
