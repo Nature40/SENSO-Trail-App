7f11f69a1e41aad034cf822e25c265132ca3bfa5
Nimo <j.hofmann.nimo@gmail.com>
2020-05-14 09:28:39 +0200

    change src urls of map

---
+++ fixMapSourcesForGithubPages.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "$PUBLIC_URL"
+
+SOURCE_URL=""
+
+if [ -n "$PUBLIC_URL" ]
+  then SOURCE_URL=${PUBLIC_URL//\//\\\/}
+  else SOURCE_URL=\/SENSO-Trail-App\/
+fi
+
+SOURCE_URL+="assets\/map"
+echo "$SOURCE_URL"
+sed "s/\/assets\/map/$SOURCE_URL/g" build/assets/map/index.html > build/assets/map/index.html
