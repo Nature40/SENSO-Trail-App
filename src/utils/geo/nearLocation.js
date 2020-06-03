778240e67423a746e1b836f22c0414caf0c8e041
Nimo <7958827+Nimo-19@users.noreply.github.com>
2020-06-03 11:14:41 +0200

    distance estimation (#49)
    * add basic distance estimation function
    
    ref: https://en.wikipedia.org/wiki/Geographical_distance#Spherical_Earth_projected_to_a_plane
    
    * add tests and distance to nearLocation function

--- src/utils/geo/nearLocation.js
+++ src/utils/geo/nearLocation.js
@@ -6,9 +6,44 @@
  * @param {number} range
  */
 
-export default function isNearLocation(xLatitude, xLongitude, locLatitude, locLongitude, range = 1.0){
-  return (
-    Math.abs(xLatitude - locLatitude) < range &&
-    Math.abs(xLongitude - locLongitude ) < range 
+export default function isNearLocation(xLatitude, xLongitude, locLatitude, locLongitude, range = 10){
+  return geoDistance(xLatitude, xLongitude, locLatitude, locLongitude) < range;
+}
+
+const EARTH_RAD_R = 6371009 //Earth Radius in meter
+
+/**
+ * @param {double} lat_1
+ * @param {double} long_1
+ * @param {double} lat_2
+ * @param {double} long_2
+ * return {double}
+ */
+export function geoDistance(lat_1, long_1, lat_2, long_2)
+{
+  lat_1 = degToRad(lat_1)
+  long_1 = degToRad(long_1)
+  lat_2 = degToRad(lat_2)
+  long_2 = degToRad(long_2)
+  const deltaLat = lat_2 - lat_1
+  const deltaLong = long_2 - long_1
+
+  const lat_mean = (lat_1 + lat_2)/2
+  
+  let helper = Math.cos(lat_mean)*deltaLong
+  helper = helper*helper
+
+  return EARTH_RAD_R * Math.sqrt(
+    (deltaLat * deltaLat) + helper
   )
+}
+
+const DEG_TO_RAD_FACTOR = Math.PI / 180
+
+/**
+ * @param {double} deg - angle in degree
+ * @return {double} - angle in radians
+ */
+function degToRad(deg){
+  return DEG_TO_RAD_FACTOR*deg
 }
